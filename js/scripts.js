1ae7990548b4a0b069a9a30e6e7fbfaad2c73fff
Taylor Somers <tsomers@yandex.com>
2020-06-08 18:39:57 -0400

    Unsuccessfully try to create a loop that will perform the same function as the previous JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
-$(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
+//   $("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
 
-  $("img").click(function {
-    alert("This is an image.");
+//   $("img").click(function {
+//     alert("This is an image.");
+//   });
+// });
+
+let array = [["h1", "header"], ["p", "paragraph"], ["img", "image"]];
+  array.forEach(function(element, display) {
+  $(element).click(function() {
+    
+    if (element === "h1" || "P") {
+    alert("This is a " + display);
+    } else {
+    alert("This is an " + display);
+    }
   });
 });
